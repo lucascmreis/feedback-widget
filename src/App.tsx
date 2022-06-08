9c0a7f5b27c0cc8dea14144f4044f74cb5d895a2
lucas.creis <lucas.creis@b2wdigital.com>
2022-06-09 01:26:12 +0200

    feat: add Widget Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { Widget } from "./components/Widget";
 
-function App() {
-  return <h1>Welcome</h1>
+export function App() {
+  return <Widget />
 }
 
-export default App
