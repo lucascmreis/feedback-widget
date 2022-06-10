b7a9bdc4008c39d451d8446165b6dcea0bedcda6
lucas.creis <lucas.creis@b2wdigital.com>
2022-06-10 23:11:47 +0200

    feat: create feedback form

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from "phosphor-react"
+import { ArrowLeft, Camera } from "phosphor-react"
 import { FeedbackType,feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 interface FeedbackContentStepProps {
@@ -25,6 +25,20 @@ export function FeedbackContentStep({feedbackType, onRestartFeedback}: FeedbackC
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin "
           placeholder="Conte com detalhes o que está acontecendo"
         />
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="button"
+            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus: ring-brand-500 "
+          >
+            <Camera className="w-6 h-6 "/>
+          </button>
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus: ring-brand-500 transition-colors"
+          >
+            Enviar feedback
+          </button>
+        </footer>
       </form>
   </>
   )
