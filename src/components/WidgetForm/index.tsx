303e4fe45e9a85dfc624f7354892d8894474cf86
lucas.creis <lucas.creis@b2wdigital.com>
2022-06-10 22:16:11 +0200

    feat: add content feedback type

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,9 +1,10 @@
 import {useState} from 'react'
 import { CloseButton } from "../CloseButton";
-import bugImageUrl from '../assets/bug.svg'
-import ideaImageUrl from '../assets/idea.svg'
-import thoughtImageUrl from '../assets/thought.svg'
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
 import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
  
 export const feedbackTypes = {
   BUG: {
@@ -32,17 +33,17 @@ export type FeedbackType = keyof typeof feedbackTypes
 
 export function WidgetForm(){
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  function handleRestartFeedback(){
+    setFeedbackType(null)
+  }
 
   return(
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto" >
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
+      
       {!feedbackType ? (
         <FeedbackTypeStep  onFeedbackTypeChanged={setFeedbackType}/>
       ): (
-        <h1>hello</h1>
+        <FeedbackContentStep feedbackType={feedbackType} onRestartFeedback={handleRestartFeedback}/>
       )}
       <footer className="text-xs text-neutral-400" >
         Made with ♥ by <a href="https://github.com/lucascmreis">Lucas Reis</a>
